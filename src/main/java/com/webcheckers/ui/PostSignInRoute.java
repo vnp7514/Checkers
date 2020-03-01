9b4690f81f2f9003c70787d7e9821f24729ec5d1
vnp7514 <vnp7514@rit.edy>
2020-02-29 22:47:54 -0500

    It is able to save a username in the playerLobby instance and the playerServices instance. If the username is already defined or invalid, there will be messages appeared in the message box of the Signin page.

--- src/main/java/com/webcheckers/ui/PostSignInRoute.java
+++ src/main/java/com/webcheckers/ui/PostSignInRoute.java
@@ -2,20 +2,40 @@ package com.webcheckers.ui;
 
 import com.webcheckers.Checkers.Player;
 import com.webcheckers.application.PlayerLobby;
+import com.webcheckers.application.PlayerServices;
 import com.webcheckers.util.Message;
 import spark.*;
 
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Objects;
+import java.util.logging.Logger;
+
+import static spark.Spark.halt;
 
 public class PostSignInRoute implements Route{
+    private static final Logger LOG = Logger.getLogger(PostSignInRoute.class.getName());
 
+
+    private final PlayerLobby playerLobby;
     private final TemplateEngine templateEngine;
 
+
+    // Messages for Username error checking
+    private static final Message USER_ALREADY_DEFINED =
+            Message.error("This username has been taken!");
+    private static final Message AT_LEAST_ALPHANUM =
+            Message.error("The name needs to contain at least one " +
+                    "alphanumeric character!");
+    private static final Message NO_ALPHANUM =
+            Message.error("The name cannot have one or more characters" +
+                    " that are not alphanumeric or spaces!");
+
+
+    // Values used in the View-Model map for rendering the page
     static final String VIEW_NAME = "signin.ftl";
-    static final String TITLE = "Sign In!";
     static final String NAME_PARAM = "username";
+
 
     /**
      * The constructor for the code {@code POST /signin} route handler.
@@ -25,28 +45,73 @@ public class PostSignInRoute implements Route{
      * @throws NullPointerException
      *     when the {@code templateEngine} is null
      */
-    public PostSignInRoute(TemplateEngine templateEngine) {
+    public PostSignInRoute(final PlayerLobby playerLobby, final TemplateEngine templateEngine) {
         //Validation
         Objects.requireNonNull(templateEngine, "templateEngine must not be null");
         //
+        this.playerLobby = Objects.requireNonNull(playerLobby, "playerLobby must not be null");
         this.templateEngine = templateEngine;
     }
 
+    /**
+     * Check whether this is a valid username
+     * @param userName the name
+     * @return null if it is a valid name. Otherwise, a specific Message
+     *         will be returned
+     */
+    private Message checkUsername(String userName){
+        //TODO
+        return null;
+    }
 
     @Override
     public Object handle(Request request, Response response) throws Exception {
+        LOG.fine("PostSignInRoute is called!");
         // View-model
         final Map<String, Object> vm = new HashMap<>();
-        // TODO need a condition statement here in case of Lobby is full or null
+        vm.put(GetHomeRoute.TITLE_ATTR, "Sign In");
+        // retrieve the HTTP connection
         final Session session = request.session();
+
         final String userName = request.queryParams(NAME_PARAM);
-        Player player = new Player(userName);
-        PlayerLobby playerLobby = session.attribute(GetHomeRoute.PLAYERLOBBY_KEY);
-        playerLobby.addPlayer(player);
-        vm.put(GetHomeRoute.TITLE_ATTR, "Welcome!");
-        vm.put(GetHomeRoute.USER_ATTR, player);
-        vm.put(GetHomeRoute.MESSAGE_ATTR, Message.info("Successfully logged in!"));
-        ModelAndView mv = new ModelAndView(vm, GetHomeRoute.VIEW_NAME);
-        return templateEngine.render(mv);
+        // Checking whether this is a valid name
+        Message message = checkUsername(userName);
+
+        if (message == null) {
+
+            LOG.fine("Username is valid!");
+            Player player = new Player(userName);
+            PlayerServices playerServices =
+                    session.attribute(GetHomeRoute.PLAYER_KEY);
+
+            if (playerServices == null) {
+                LOG.fine("PlayerServices is null");
+                response.redirect(WebServer.HOME_URL);
+                halt("PlayerServices is null");
+                return null;
+            } else {
+                LOG.fine("PlayerServices is not null");
+                if (playerServices.getPlayer() == null) {
+                    LOG.fine("This session has not signed in!");
+                    if (playerLobby.containPlayer(player)) {
+                        vm.put(GetHomeRoute.MESSAGE_ATTR, USER_ALREADY_DEFINED);
+                    } else {
+                        playerLobby.addPlayer(player);
+                        playerServices.insertPlayer(player);
+                        response.redirect(WebServer.HOME_URL);
+                        halt();
+                        return null;
+                    }
+                } else {
+                    LOG.fine("This session has signed in!");
+                    response.redirect(WebServer.HOME_URL);
+                    halt();
+                    return null;
+                }
+            }
+        } else {
+            vm.put(GetHomeRoute.MESSAGE_ATTR, message);
+        }
+        return templateEngine.render(new ModelAndView(vm, VIEW_NAME));
     }
 }
