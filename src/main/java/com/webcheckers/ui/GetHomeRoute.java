24345eabcb7106e7dc01aed81314ff2f60efcafb
vnp7514 <vnp7514@rit.edy>
2020-03-25 22:18:57 -0400

    GetHomeRoute class is modified so that strings are declared at the top. Test Suite made for GetHomeRoute class.

--- src/main/java/com/webcheckers/ui/GetHomeRoute.java
+++ src/main/java/com/webcheckers/ui/GetHomeRoute.java
@@ -20,7 +20,7 @@ import static spark.Spark.halt;
 public class GetHomeRoute implements Route {
     private static final Logger LOG = Logger.getLogger(GetHomeRoute.class.getName());
 
-    private static final Message WELCOME_MSG = Message.info("Welcome to the world of online Checkers.");
+    public static final Message WELCOME_MSG = Message.info("Welcome to the world of online Checkers.");
 
     // The length of the session timeout in seconds
     static final int SESSION_TIMEOUT_PERIOD = 120;
@@ -38,8 +38,9 @@ public class GetHomeRoute implements Route {
     static final String USER_ATTR = "currentUser";
     static final String PLAYER_LIST_ATTR = "players";
     static final String VIEW_NAME = "home.ftl";
-    static final String ACTIVE_USERS = "otherUsers";
-    static final String PLAYER_STATUS = "playerStatus";
+    static final String ACTIVE_USERS_ATTR = "otherUsers";
+
+    static final String WELCOME = "Welcome!";
 
     /**
      * Create the Spark Route (UI controller) to handle all {@code GET /} HTTP requests.
@@ -70,14 +71,14 @@ public class GetHomeRoute implements Route {
         LOG.fine("GetHomeRoute is invoked.");
         // View-Model
         Map<String, Object> vm = new HashMap<>();
-        vm.put(TITLE_ATTR, "Welcome!");
+        vm.put(TITLE_ATTR, WELCOME);
         // display a user message in the Home page
         vm.put(MESSAGE_ATTR, WELCOME_MSG);
 
         // Retrieve the HTTP session
         final Session httpSession = request.session();
 
-        vm.put(ACTIVE_USERS, String.format("There are %d users online.", playerLobby.lobbySize()));
+        vm.put(ACTIVE_USERS_ATTR, String.format("There are %d users online.", playerLobby.lobbySize()));
 
 
 
@@ -103,7 +104,7 @@ public class GetHomeRoute implements Route {
                 // if the user has signed in with a username
                 LOG.fine("A signed in player!");
                 vm.put(USER_ATTR, playerServices.getPlayer());
-                vm.put(ACTIVE_USERS, "");
+                vm.put(ACTIVE_USERS_ATTR, "");
 
                 //If there is a message for the player from the server
                 if (playerServices.getMessage() != null) {
---
+++ src/test/java/com/webcheckers/ui/GetHomeRouteTest.java
@@ -0,0 +1,176 @@
+package com.webcheckers.ui;
+
+import com.webcheckers.Checkers.Player;
+import com.webcheckers.application.PlayerLobby;
+import com.webcheckers.application.PlayerServices;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+import spark.*;
+
+import java.util.List;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.*;
+import static org.mockito.Mockito.verify;
+
+/**
+ * The unit test suite for the {@Link GetHomeRoute} component.
+ * @author Van Pham
+ */
+@Tag("UI-tier")
+public class GetHomeRouteTest {
+    /**
+     * The component under test (CuT)
+     *
+     * This is a stateless component so we only need one.
+     */
+    private GetHomeRoute CuT;
+
+    // Hopefully friendly object TODO
+    private PlayerLobby playerLobby;
+
+    // Friendly objects
+    Player p1 = new Player("Player 1");
+    Player p2 = new Player("Player 2");
+    Player p3 = new Player("Player 3");
+    Player p4 = new Player("Player 4");
+
+    // mock objects
+    private Request request;
+    private TemplateEngine templateEngine;
+    private Session session;
+    private Response response;
+
+    /**
+     * Setup new mock objects for each test
+     */
+    @BeforeEach
+    public void setup(){
+        request = mock(Request.class);
+        session = mock(Session.class);
+        response = mock(Response.class);
+        templateEngine = mock(TemplateEngine.class);
+        when(request.session()).thenReturn(session);
+
+
+        // create a unique CuT for each test
+        // the PlayerLobby is friendly but the engine mock will need configuration
+        playerLobby = new PlayerLobby();
+        CuT = new GetHomeRoute(playerLobby, templateEngine);
+    }
+
+    /**
+     * Test that CuT shows the Home view when the session is brand new.
+     */
+    @Test
+    public void new_session() {
+        // To analyze what the Route created in the View-Model map you need
+        // to be able to extract the argument to the TemplateEngine.render method.
+        // Mock up the 'render' method by supplying a Mockito 'Answer' object
+        // that captures the ModelAndView data passed to the template engine
+        final TemplateEngineTester testHelper = new TemplateEngineTester();
+        when(templateEngine.render(any(ModelAndView.class))).thenAnswer(testHelper.makeAnswer());
+
+        // Invoke the test
+        CuT.handle(request, response);
+
+        // Analyze the results:
+        //   * model is a non-null Map
+        testHelper.assertViewModelExists();
+        testHelper.assertViewModelIsaMap();
+        //   * model contains all necessary View-Model data
+        testHelper.assertViewModelAttribute(GetHomeRoute.TITLE_ATTR, GetHomeRoute.WELCOME);
+        testHelper.assertViewModelAttribute(GetHomeRoute.MESSAGE_ATTR, GetHomeRoute.WELCOME_MSG);
+        testHelper.assertViewModelAttribute(GetHomeRoute.ACTIVE_USERS_ATTR,
+                String.format("There are %d users online.", playerLobby.lobbySize()));
+        testHelper.assertViewModelAttribute(GetHomeRoute.USER_ATTR, null);
+        testHelper.assertViewModelAttribute(GetHomeRoute.PLAYER_LIST_ATTR, null);
+
+
+        //   * test view name
+        testHelper.assertViewName(GetHomeRoute.VIEW_NAME);
+        //   * verify that a player service object and the session timeout watchdog are stored
+        //   * in the session.
+        verify(session).attribute(eq(GetHomeRoute.PLAYER_KEY), any(PlayerServices.class));
+        verify(session).attribute(eq(GetHomeRoute.TIMEOUT_SESSION_KEY),
+                any(SessionTimeoutWatchdog.class));
+    }
+
+    /**
+     * Test that CuT shows the Home view when the session is brand new
+     *     and there are some people who have signed in.
+     */
+    @Test
+    public void new_session_with_signin() {
+        // To analyze what the Route created in the View-Model map you need
+        // to be able to extract the argument to the TemplateEngine.render method.
+        // Mock up the 'render' method by supplying a Mockito 'Answer' object
+        // that captures the ModelAndView data passed to the template engine
+        final TemplateEngineTester testHelper = new TemplateEngineTester();
+        when(templateEngine.render(any(ModelAndView.class))).thenAnswer(testHelper.makeAnswer());
+
+        playerLobby.addPlayer(p1);
+        // Invoke the test
+        CuT.handle(request, response);
+
+        // Analyze the results:
+        //   * model is a non-null Map
+        testHelper.assertViewModelExists();
+        testHelper.assertViewModelIsaMap();
+        //   * model contains all necessary View-Model data
+        testHelper.assertViewModelAttribute(GetHomeRoute.TITLE_ATTR, GetHomeRoute.WELCOME);
+        testHelper.assertViewModelAttribute(GetHomeRoute.MESSAGE_ATTR, GetHomeRoute.WELCOME_MSG);
+        testHelper.assertViewModelAttribute(GetHomeRoute.ACTIVE_USERS_ATTR,
+                String.format("There are %d users online.", 1));
+        testHelper.assertViewModelAttribute(GetHomeRoute.USER_ATTR, null);
+        testHelper.assertViewModelAttribute(GetHomeRoute.PLAYER_LIST_ATTR, null);
+
+
+        //   * test view name
+        testHelper.assertViewName(GetHomeRoute.VIEW_NAME);
+        //   * verify that a player service object and the session timeout watchdog are stored
+        //   * in the session.
+        verify(session).attribute(eq(GetHomeRoute.PLAYER_KEY), any(PlayerServices.class));
+        verify(session).attribute(eq(GetHomeRoute.TIMEOUT_SESSION_KEY),
+                any(SessionTimeoutWatchdog.class));
+    }
+    /**
+     * Test the Home View when the player has just signed in
+     */
+    @Test
+    public void sign_in_session(){
+        final TemplateEngineTester testHelper = new TemplateEngineTester();
+        when(templateEngine.render(any(ModelAndView.class))).thenAnswer(testHelper.makeAnswer());
+
+        // Mocking a signed in player
+        playerLobby.addPlayer(p1);
+        final PlayerServices playerService = playerLobby.newPlayerServices();
+        playerService.insertPlayer(p1);
+        session.attribute(GetHomeRoute.PLAYER_KEY, playerService);
+        session.attribute(GetHomeRoute.TIMEOUT_SESSION_KEY, new SessionTimeoutWatchdog(playerService));
+        session.maxInactiveInterval(GetHomeRoute.SESSION_TIMEOUT_PERIOD);
+        when(session.attribute(GetHomeRoute.PLAYER_KEY)).thenReturn(playerService);
+
+        // Invoke the test
+        CuT.handle(request, response);
+
+
+        // Analyze the results:
+        //   * model is a non-null Map
+        testHelper.assertViewModelExists();
+        testHelper.assertViewModelIsaMap();
+        //   * model contains all necessary View-Model data
+        testHelper.assertViewModelAttribute(GetHomeRoute.TITLE_ATTR, GetHomeRoute.WELCOME);
+        testHelper.assertViewModelAttribute(GetHomeRoute.MESSAGE_ATTR, GetHomeRoute.WELCOME_MSG);
+        testHelper.assertViewModelAttribute(GetHomeRoute.ACTIVE_USERS_ATTR, "");
+        testHelper.assertViewModelAttribute(GetHomeRoute.USER_ATTR, playerService.getPlayer());
+        List<String> players = playerLobby.availablePlayers();
+        players.remove(playerService.getPlayer().getName());
+        testHelper.assertViewModelAttribute(GetHomeRoute.PLAYER_LIST_ATTR, players);
+
+        //   * test view name
+        testHelper.assertViewName(GetHomeRoute.VIEW_NAME);
+    }
+}
