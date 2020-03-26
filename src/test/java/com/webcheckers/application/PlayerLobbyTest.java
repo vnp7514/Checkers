bf260f39ee63a00f8f7c509256f9a5b12fb03cb1
vnp7514 <vnp7514@rit.edy>
2020-03-25 20:41:04 -0400

    Test suite for PlayerLobby

---
+++ src/test/java/com/webcheckers/application/PlayerLobbyTest.java
@@ -0,0 +1,111 @@
+package com.webcheckers.application;
+
+import com.webcheckers.Checkers.Player;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * The unit test suite for the {@Link PlayerLobby} component.
+ *
+ * @author Van Pham
+ */
+@Tag("Application-tier")
+public class PlayerLobbyTest {
+
+    // Objects used for testing
+    String[] three_names = new String[] {"Player 1","Player 2","Player 3"};
+    String[] zero_names = new String[] {};
+    String[] one_names = new String[] {"Player 1"};
+
+
+    // Friendly objects
+    Player p1 = new Player("Player 1");
+    Player p2 = new Player("Player 2");
+    Player p3 = new Player("Player 3");
+    Player p4 = new Player("Player 4");
+    GameLobby gameLobby = new GameLobby(p1);
+
+
+    /**
+     * Test the ability to make a new PlayerService.
+     */
+    @Test
+    public void test_make_player_service() {
+        final PlayerLobby CuT = new PlayerLobby();
+        // Invoke test
+        final PlayerServices playerSvc = CuT.newPlayerServices();
+        // Analyze results
+        assertNotNull(playerSvc);
+    }
+
+    /**
+     * Test the ability to add a player to the lobby, delete
+     *     a player from the lobby, and whether
+     *     the lobby correctly remembers the player
+     */
+    @Test
+    public void test_add_player(){
+        final PlayerLobby CuT = new PlayerLobby();
+        assertFalse(CuT.containPlayer(p1));
+        fail("containPlayer() is flawed");
+        CuT.addPlayer(p1);
+        assertTrue(CuT.containPlayer(p1), "addPlayer() is flawed");
+        CuT.removePlayer(p1);
+        assertFalse(CuT.containPlayer(p1), "removePlayer() is flawed");
+    }
+
+    /**
+     * Test the ability to correctly count the number of players in the lobby
+     */
+    @Test
+    public void test_lobby_size(){
+        final PlayerLobby CuT = new PlayerLobby();
+        assertEquals(0, CuT.lobbySize(), "Lobby was not initially zero");
+        CuT.addPlayer(p1);
+        assertEquals(1, CuT.lobbySize(), "addPlayer did not change the lobby size");
+        CuT.addPlayer(p2);
+        assertEquals(2, CuT.lobbySize(), "addPlayer did not add a player");
+        CuT.removePlayer(p1);
+        assertEquals(1, CuT.lobbySize(), "removePlayer is not reflected on the lobbySize");
+    }
+
+    /**
+     * Test the ability to create a list of available Player names (aka String)
+     */
+    @Test
+    public void test_availablePlayers(){
+        final PlayerLobby CuT = new PlayerLobby();
+        boolean rlt = Arrays.equals(zero_names, CuT.availablePlayers().toArray(new String[0]));
+        assertTrue(rlt);
+        CuT.addPlayer(p1);
+        rlt = Arrays.equals(one_names, CuT.availablePlayers().toArray(new String[0]));
+        assertTrue(rlt);
+        CuT.addPlayer(p2);
+        CuT.addPlayer(p3);
+        rlt = Arrays.equals(three_names, CuT.availablePlayers().toArray(new String[0]));
+        assertTrue(rlt);
+    }
+
+    /**
+     * Test addGame and removeGame. See whether the PlayerLobby knows that
+     *     A player is in a game
+     */
+    @Test
+    public void test_gameLobby(){
+        final PlayerLobby CuT = new PlayerLobby();
+        assertFalse(CuT.playerInGame(p1), "playerInGame has an error");
+        CuT.addGame(gameLobby);
+        assertTrue(CuT.playerInGame(p1), "player 1 should be in a game");
+        assertEquals(CuT.playerOfGame(p1), gameLobby, "playerOfGame is broken");
+        CuT.removeGame(gameLobby);
+        assertFalse(CuT.playerInGame(p1));
+        assertNull(CuT.playerOfGame(p1), "p1 is in a gameLobby");
+    }
+
+
+}
