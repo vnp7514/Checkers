a12103140d37845d546185f4c84d35e45a77ade4
vnp7514 <vnp7514@rit.edy>
2020-04-20 21:25:48 -0400

    GameLobby test suites

--- src/test/java/com/webcheckers/appl/GameLobbyTest.java
+++ src/test/java/com/webcheckers/appl/GameLobbyTest.java
@@ -1,5 +1,6 @@
 package com.webcheckers.appl;
 
+import com.webcheckers.model.Color;
 import com.webcheckers.model.Player;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
@@ -85,6 +86,62 @@ public class GameLobbyTest {
         assertTrue((test1.containPlayer(p2)));
         assertNotEquals(p1, test1.getWhitePlayer());
         assertEquals(p2, test1.getWhitePlayer());
+    }
+
+    @Test
+    public void runningTest(){
+        GameLobby test1 = new GameLobby();
+        test1.addPlayer(new Player("joe"));
+        test1.addPlayer(new Player("john"));
+        assertTrue(test1.isRunning());
+        assertEquals(Color.RED,test1.getActiveColor());
+        test1.end();
+        assertEquals(Color.RED, test1.getActiveColor());
+        assertFalse(test1.isRunning());
+        test1 = new GameLobby();
+        test1.addPlayer(new Player("joe"));
+        test1.addPlayer(new Player("john"));
+        test1.resign(new Player("joe"));
+        assertEquals(new Player("joe"), test1.getQuitter());
+        assertFalse(test1.isRunning());
+        assertFalse(test1.containPlayer(new Player("joe")));
+        assertTrue(test1.containPlayer(new Player("john")));
+
+        test1 = new GameLobby();
+        test1.swapActiveColor();
+        assertEquals(Color.WHITE, test1.getActiveColor());
+
+        test1 = new GameLobby(new Player("joe"), 1);
+        assertEquals(1, test1.getGameID());
+        test1.addPlayer(new Player("john"));
+        Player p1 = new Player("p1");
+        Player p2 = new Player("p2");
+        assertFalse(test1.hasSpectator());
+        test1.addSpectator(p1);
+        assertTrue(test1.hasSpectator());
+        test1.removeSpectator(p1);
+        assertFalse(test1.hasSpectator());
+        test1.addSpectator(p1);
+        assertTrue(test1.containSpectator(p1));
+        assertFalse(test1.containSpectator(p2));
+    }
+
+    @Test
+    public void testEquals(){
+        GameLobby test1 = new GameLobby(new Player("joe"),1);
+        test1.addPlayer(new Player("jone"));
+        GameLobby test2 = new GameLobby(new Player("joe"),1);
+        test2.addPlayer(new Player("jone"));
+        assertTrue(test1.equals(test2));
+        assertFalse(test1.equals(1));
+    }
+
+    @Test
+    public void testToString(){
+        final GameLobby test1 = new GameLobby();
+        test1.addPlayer(new Player("john"));
+        test1.addPlayer(new Player("bitc"));
+        assertNotNull(test1.toString());
     }
 
     /**
